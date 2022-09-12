4ea890a8674c11f571200ac340365a1e32e0499b
Youssef Ahmed <youssefashwal@outlook.com>
2022-09-13 00:42:38 +0200

    added navbar creating function

--- website/js/app.js
+++ website/js/app.js
@@ -1,11 +1,22 @@
 // Global Variables
 
+let sections = Array.from(document.querySelectorAll('section'));
+const navList = document.getElementById('navbar__list');
 
 // Helper Functions 
 
 // Main Functions
 
+// creates nav bar dynamically
+function createNavBar() {
+    for (let i = 1; i <= sections.length; i++) {
+        listItem = document.createElement('li');
+        listItem.setAttribute('class', 'navbar__elements');
+        listItem.innerHTML = `<a href="#section${i}">Section ${i}</a>`;
+        navList.appendChild(listItem);
+    }
+}
 
 // Events
 
-
+createNavBar();
